107f4cab2d442139f9db0250963675170667fdb9
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2022-12-31 19:31:30 -0500

    The Inspectorate

--- JS/group.js
+++ JS/group.js
@@ -208,9 +208,9 @@ class group{
         }
         if(this.calc.list.length>0){
             h=this.calc.list[floor(random(0,this.calc.list.length))]
-            if(this.cards[this.calc.list[h]].level==0){
-                this.cards[this.calc.list[h]].level++
-                this.cards[this.calc.list[h]]=reformCard(this.cards[h])
+            if(this.cards[h].level==0){
+                this.cards[h].level++
+                this.cards[h]=reformCard(this.cards[h])
             }
         }
     }
